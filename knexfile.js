54da2b2299db4fb16ab7c222b262966c0a9f4707
spettigrew <sara.pettigrew5@gmail.com>
2020-01-29 16:10:17 -0700

    Placed testing above staging and production

--- knexfile.js
+++ knexfile.js
@@ -15,6 +15,20 @@ module.exports = {
     seeds: { directory: './database/seeds' },
   },
 
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/test.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
+  
   staging: {
     client: 'postgresql',
     connection: {
@@ -45,19 +59,5 @@ module.exports = {
     migrations: {
       tableName: 'knex_migrations'
     }
-  },
-  
-  testing: {
-    client: 'sqlite3',
-    connection: {
-      filename: './database/test.db3',
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './database/migrations',
-    },
-    seeds: {
-      directory: './database/seeds',
-    },
   },
 };
