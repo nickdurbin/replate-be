fd7e6a7a60667376e00ddb5df9012ddb080c912b
spettigrew <sara.pettigrew5@gmail.com>
2020-01-29 21:20:08 -0700

    Removed null() and primary key

--- database/migrations/20200129184741_food_request.js
+++ database/migrations/20200129184741_food_request.js
@@ -3,10 +3,10 @@ exports.up = async function(knex) {
   await knex.schema.createTable("food-request", (table) => {
       table.increments("id")
       table.string("type", 280).notNullable
-      table.float("amount-servings", 280)(null)
-      table.date("preferred pickup-date")(null)
-      table.time("preferred pickup-time")(null)
-      table.string("description", 500)(null)
+      table.float("amount-servings", 280)
+      table.date("preferred pickup-date")
+      table.time("preferred pickup-time")
+      table.string("description", 500)
       table.boolean('completed').defaultTo(false);
         table.integer("business_id")
         .notNullable()
@@ -21,7 +21,7 @@ exports.up = async function(knex) {
         .inTable("volunteer")
         .onDelete("CASCADE")
         .onUpdate("CASCADE")
-    table.primary(["business_id", "food-request_id"])
+    // table.primary(["business_id", "food-request_id"])
   })
 };
 
