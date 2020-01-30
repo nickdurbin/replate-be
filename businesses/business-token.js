8023dea3e33c408cf29d09a32e8ca856fcfcf8ab
spettigrew <sara.pettigrew5@gmail.com>
2020-01-30 16:56:48 -0700

    Business token file setup

---
+++ businesses/business-token.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken")
+const config = require("../config/jwtConfig")
+
+function signToken(businessUser) {
+    const payload = {
+        subject: businessUser.id,
+        username: businessUser.username,
+        role: "donator",
+    }
+
+    return jwt.sign(payload, config.jwtSecret, config.options)
+}
+
+module.exports = signToken
