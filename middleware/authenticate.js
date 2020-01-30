06ac91f560dcbefbfc4d66590fb17f28143e75cc
spettigrew <sara.pettigrew5@gmail.com>
2020-01-29 20:01:15 -0700

    Changed error message

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -15,6 +15,6 @@ module.exports = (req, res, next) => {
         })
 
     } else {
-        return res.status(401).json({ message: "You are not authorized" })
+        return res.status(401).json({ message: "You may not continue" })
     }
 }
