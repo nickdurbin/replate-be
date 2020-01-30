ddf007a769a34d6f3fb4e9fd56638fdfe5989d4f
spettigrew <sara.pettigrew5@gmail.com>
2020-01-29 20:09:52 -0700

    Changed some tables to null()

--- database/migrations/20200129184512_business.js
+++ database/migrations/20200129184512_business.js
@@ -4,9 +4,9 @@ exports.up = async function(knex) {
       table.increments("id")
       table.string("username", 280).notNullable().unique()
       table.string("password", 280).notNullable()
-      table.string("name", 280).notNullable()
-      table.string("address", 280).notNullable()
-      table.integer("phone-number", 10).notNullable.unique()
+      table.string("name", 280).null()
+      table.string("address", 280).null()
+      table.integer("phone-number", 10).null.unique()
       table.string("email", 280).null()
   })
 };
