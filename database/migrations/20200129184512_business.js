7b433b7c54fd45558ba7c6a52df416c677081437
spettigrew <sara.pettigrew5@gmail.com>
2020-01-29 19:59:34 -0700

    Changed table names

--- database/migrations/20200129184512_business.js
+++ database/migrations/20200129184512_business.js
@@ -4,10 +4,10 @@ exports.up = async function(knex) {
       table.increments("id")
       table.string("username", 280).notNullable().unique()
       table.string("password", 280).notNullable()
-      table.string("business-name", 280).notNullable()
-      table.string("business-address", 280).notNullable()
-      table.integer("business-phone-number", 10).notNullable.unique()
-      table.string("business-email", 280).null()
+      table.string("name", 280).notNullable()
+      table.string("address", 280).notNullable()
+      table.integer("phone-number", 10).notNullable.unique()
+      table.string("email", 280).null()
   })
 };
 
