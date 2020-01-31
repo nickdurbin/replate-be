5346de26658f9a5864d278dd1c8fe3508f136ffc
spettigrew <sara.pettigrew5@gmail.com>
2020-01-30 19:26:23 -0700

    Food model spec file setup, tests not passing

---
+++ foodRequest/foodModel.spec.js
@@ -0,0 +1,32 @@
+const db = require("../database/dbConfig")
+const FoodModel = require("../foodRequest/food-model")
+
+beforeEach(async () => {
+    await db.seed.run()
+})
+
+describe("food model list", () => {
+    test("list", async () => {
+        const res = await FoodModel.list()
+        expect(res).toBeGreaterThan[1]
+    })
+})
+
+describe("find food request in model", () => {
+    test("findById", async () => {
+        const res = await FoodModel.findById(1)
+        expect(res.type).toBe("soups")
+    })
+
+    test("update food request", async () => {
+        await VolunteerModel.update(1, { name: "salads" })
+        const food = await VolunteerModel.findById(1)
+        expect(food.name).toBe("salads")
+    })
+
+    test("remove food request", async () => {
+        await FoodModel.remove(1)
+        const food = await FoodModel.list()
+        expect(food).toHaveLength(2)
+    })
+})
