52a2f32d8d2ad947481d88a687a7802c199cc6b8
spettigrew <sara.pettigrew5@gmail.com>
2020-01-31 19:22:21 -0700

    Food seed file setup

--- database/seeds/003_food_request.js
+++ database/seeds/003_food_request.js
@@ -1,13 +1,16 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = async (knex) => {
+
+  await knex("users").truncate()
+
+  await knex("users").insert([
+    { id: 1, type: "Soup", amount_servings: 2, pickup_time: 1581066814, description: "Describe food to be picked up.", completed: false, business_id: 1, volunteer_id: 1},
+    { id: 2, type: "Salad", amount_servings: 4, pickup_time: 1581498814, description: "Describe food to be picked up.", completed: false, business_id: 3, volunteer_id: 3 },
+    { id: 3, type: "Fruit/Vegetable", amount_servings: 12, pickup_time: 1583741614, description: "Describe food to be picked up.", completed: false, business_id: 5, volunteer_id: 5 },
+    { id: 4, type: "Side Dish", amount_servings: 20, pickup_time: 1581239614, description: "Describe food to be picked up.", completed: false, business_id: 7, volunteer_id: 7 },
+    { id: 5, type: "Entree/Main Dish", amount_servings: 20, pickup_time: 1580894014, description: "Describe food to be picked up.", completed: false, business_id: 9, volunteer_id: 9 },
+    { id: 6, type: "Bread", amount_servings: 20, pickup_time: 1581758014, description: "Describe food to be picked up.", completed: false, business_id: 9, volunteer_id: 2 },
+    { id: 7, type: "Dessert", amount_servings: 6, pickup_time: 1581671614, description: "Describe food to be picked up.", completed: false, business_id: 9, volunteer_id: 4 },
+    { id: 8, type: "Beverages", amount_servings: 12, pickup_time: 1584173614, description: "Describe food to be picked up.", completed: false, business_id: 9, volunteer_id: 6 }
+  ])
 };
