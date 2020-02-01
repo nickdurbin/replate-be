5445995815aabdbf72f5d8f692ee20fcb78e04ba
spettigrew <sara.pettigrew5@gmail.com>
2020-01-31 17:56:29 -0700

    Volunteer seed file setup

--- database/seeds/002_volunteers.js
+++ database/seeds/002_volunteers.js
@@ -1,13 +1,19 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = async (knex) => {
+
+  await knex("users").truncate()
+
+  await knex("users").insert([
+    { id: 1, username: 'joseph', password: "abc123", name: "Joseph", phone: "(877)555-1234", },
+    { id: 2, username: 'chrisd', password: "abc123", name: "Chris D.", phone: "(877)555-1234", },
+    { id: 3, username: 'chrisb', password: "abc123", name: "Chris B.", phone: "(877)555-1234", },
+    { id: 4, username: 'chriss', password: "abc123", name: "Chris S.", phone: "(877)555-1234", },
+    { id: 5, username: 'sara', password: "abc123", name: "Sara", phone: "(877)555-1234", },
+    { id: 6, username: 'mercy', password: "abc123", name: "Mercy", phone: "(877)555-1234", },
+    { id: 7, username: 'erin', password: "abc123", name: "Erin", phone: "(877)555-1234", },
+    { id: 8, username: 'lambda', password: "abc123", name: "Austen", phone: "(877)555-1234", },
+    { id: 9, username: 'hobbits', password: "abc123", name: "Frodo", phone: "(877)555-1234", },
+    { id: 10, username: 'wookie', password: "abc123", name: "Chewie", phone: "(877)555-1234", },
+  ]);
 };
+
