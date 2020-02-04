a06786c3f499c75df691949b2838a4de053901d9
spettigrew <sara.pettigrew5@gmail.com>
2020-02-03 20:31:18 -0700

    Changed dash to underscores

--- foodRequest/food-model.js
+++ foodRequest/food-model.js
@@ -1,7 +1,7 @@
 const db = require("../database/dbConfig")
 
 function list() {
-    return db("food-request")
+    return db("food_request")
     .select("type", "servings", "pickup_time", "description")
 }
 
@@ -11,20 +11,20 @@ function findBy(filter) {
 }
 
 function findById(id) {
-    return db("food-request")
+    return db("food_request")
         .where({ id })
         .first()
 }
 
 function insert(user) {
-    return db("food-request")
+    return db("food_request")
         .insert(user)
         .returning("id")
     //return findById(id)
 }
 
 function update(id, changes) {
-    return db("food-request")
+    return db("food_request")
         .where({ id })
         .update(changes)
         .returning("id")
@@ -32,7 +32,7 @@ function update(id, changes) {
 }
 
 function remove(id) {
-    return db("food-request")
+    return db("food_request")
         .where({ id })
         .del()
 }
