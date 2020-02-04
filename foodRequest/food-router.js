796bd4ee05661ce14b4243ffab60d6b72e6a0708
spettigrew <sara.pettigrew5@gmail.com>
2020-02-04 14:40:50 -0700

    Fixed typo

--- foodRequest/food-router.js
+++ foodRequest/food-router.js
@@ -1,14 +1,13 @@
-
 const FoodModel = require("../foodRequest/food-model")
 
 const router = require("express").Router()
 
-router.get("/", async (res, req, next) => {
+router.get("/", async (req, res, next) => {
     try{
         const foodRequest = await FoodModel.list(req.body)
 
         return res.status(200).json(foodRequest)
-        
+
     } catch (err) {
         next(err)
     }
