37b251195a178da20784491ab4fc7ff7f533c465
spettigrew <sara.pettigrew5@gmail.com>
2020-02-03 17:10:23 -0700

    Changed from async/await function

--- database/seeds/002_volunteers.js
+++ database/seeds/002_volunteers.js
@@ -1,19 +1,25 @@
 
-exports.seed = async (knex) => {
+exports.seed = function(knex) {
 
-  await knex("volunteer").truncate()
+  return knex("volunteer")
+  .then(
+    function() {
 
-  await knex("volunteer").insert([
-    { id: 1, username: 'joseph', password: "abc123", name: "Joseph", phone: "(877)555-1234", },
-    { id: 2, username: 'chrisd', password: "abc123", name: "Chris D.", phone: "(877)555-1234", },
-    { id: 3, username: 'chrisb', password: "abc123", name: "Chris B.", phone: "(877)555-1234", },
-    { id: 4, username: 'chriss', password: "abc123", name: "Chris S.", phone: "(877)555-1234", },
-    { id: 5, username: 'sara', password: "abc123", name: "Sara", phone: "(877)555-1234", },
-    { id: 6, username: 'mercy', password: "abc123", name: "Mercy", phone: "(877)555-1234", },
-    { id: 7, username: 'erin', password: "abc123", name: "Erin", phone: "(877)555-1234", },
-    { id: 8, username: 'lambda', password: "abc123", name: "Austen", phone: "(877)555-1234", },
-    { id: 9, username: 'hobbits', password: "abc123", name: "Frodo", phone: "(877)555-1234", },
-    { id: 10, username: 'wookie', password: "abc123", name: "Chewie", phone: "(877)555-1234", },
-  ]);
+      return knex("volunteer")
+      
+      .insert([
+      { id: 1, username: 'joseph', password: "abc123", name: "Joseph", phone: "(877)555-1234", },
+      { id: 2, username: 'chrisd', password: "abc123", name: "Chris D.", phone: "(877)555-1234", },
+      { id: 3, username: 'chrisb', password: "abc123", name: "Chris B.", phone: "(877)555-1234", },
+      { id: 4, username: 'chriss', password: "abc123", name: "Chris S.", phone: "(877)555-1234", },
+      { id: 5, username: 'sara', password: "abc123", name: "Sara", phone: "(877)555-1234", },
+      { id: 6, username: 'mercy', password: "abc123", name: "Mercy", phone: "(877)555-1234", },
+      { id: 7, username: 'erin', password: "abc123", name: "Erin", phone: "(877)555-1234", },
+      { id: 8, username: 'lambda', password: "abc123", name: "Austen", phone: "(877)555-1234", },
+      { id: 9, username: 'hobbits', password: "abc123", name: "Frodo", phone: "(877)555-1234", },
+      { id: 10, username: 'wookie', password: "abc123", name: "Chewie", phone: "(877)555-1234", },
+      ])
+    }
+  )
 };
 
