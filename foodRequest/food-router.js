13f294b8b5d447103abbb03ecd9655faf9586cf7
spettigrew <sara.pettigrew5@gmail.com>
2020-02-04 18:47:13 -0700

    Removed duplicate .get

--- foodRequest/food-router.js
+++ foodRequest/food-router.js
@@ -13,14 +13,6 @@ router.get("/", async (req, res, next) => {
     }
 })
 
-router.get("/", async (req, res, next) => {
-    try {
-        return res.json(await FoodModel.findBy())
-    }
-    catch (err) {
-        next(err)
-    }
-})
 
 router.get("/:id", async (req, res, next) => {
     try {
